3b76df20f1654baf42128ba2a2d634d7c7d3bdbf
PaulKillian <psk65lava@gmail.com>
2020-08-11 15:56:54 -0700

    requesting data

---
+++ components/grade-table.js
@@ -0,0 +1,9 @@
+class GradeTable {
+    constructor(tableElement) {
+        this.tableElement
+    }
+
+    updateGrades(grades) {
+        console.log(grades)
+    }
+}
---
+++ main.js
@@ -0,0 +1,6 @@
+var table = document.querySelector("table")
+
+var newTable = new GradeTable(table)
+
+var newApp = new App(newTable)
+newApp.start()
