742b270309a0f5a87f1f7fa1f80fcb67437a8da2
PaulKillian <psk65lava@gmail.com>
2020-08-11 17:00:04 -0700

    modified the HTML

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var table = document.querySelector("table")
-
 var newTable = new GradeTable(table)
+var header = document.querySelector("header")
+var newHeader = new PageHeader(header)
 
-var newApp = new App(newTable)
+var newApp = new App(newTable, newHeader)
 newApp.start()
